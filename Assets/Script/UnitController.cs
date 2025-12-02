9eb7e62876f764829dfacc91e56535385211a687
Burakbel <225040052@stu.istinye.edu.tr>
2025-12-03 00:08:19 +0300

    Update

--- Assets/Script/UnitController.cs
+++ Assets/Script/UnitController.cs
@@ -15,35 +15,45 @@ public class UnitController : MonoBehaviour
     public LayerMask enemyMask;
 
     private float attackTimer;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         attackTimer -= Time.deltaTime;
-
+        float moveAmount = 0f;
         Collider2D enemy = Physics2D.OverlapCircle(transform.position + Vector3.right * attackRange, 0.3f, enemyMask);
-
         if (enemy != null)
         {
             TryAttack(enemy.GetComponent<UnitHealth>());
+            SetWalking(false);
             return;
         }
+
 
         Collider2D ally = Physics2D.OverlapCircle(transform.position + Vector3.right * followDistance, 0.3f, allyMask);
 
         if (ally != null)
         {
-
-            transform.Translate(Vector3.right * (moveSpeed * slowDownFactor) * Time.deltaTime);
-
+            moveAmount = moveSpeed * slowDownFactor;
+            transform.Translate(Vector3.right * moveAmount * Time.deltaTime);
             Vector3 pushDir = (transform.position - ally.transform.position).normalized;
             transform.position += pushDir * pushBackStrength * Time.deltaTime;
-
+            SetWalking(moveAmount > 0.01f);
             return;
         }
 
 
-        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        moveAmount = moveSpeed;
+        transform.Translate(Vector3.right * moveAmount * Time.deltaTime);
+
+        SetWalking(moveAmount > 0.01f);
     }
+
     void TryAttack(UnitHealth target)
     {
         if (target == null) return;
@@ -53,6 +63,12 @@ public class UnitController : MonoBehaviour
             attackTimer = attackCooldown;
             target.TakeDamage(damage);
         }
+    }
+
+    void SetWalking(bool state)
+    {
+        if (anim != null)
+            anim.SetBool("isWalking", state);
     }
 
     private void OnDrawGizmos()
