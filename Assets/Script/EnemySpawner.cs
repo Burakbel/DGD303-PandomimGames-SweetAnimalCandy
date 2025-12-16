8e9376589d8a7e43c6a537f617f5e988e82239a4
Burakbel <225040052@stu.istinye.edu.tr>
2025-12-17 00:25:37 +0300

    Week10

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -3,28 +3,79 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public Transform spawnPoint;
+    public Transform spawnPointTest;
     public GameObject enemyA;
+    public int enemyA_Cost;
     public GameObject enemyB;
+    public int enemyB_Cost;
     public GameObject enemyC;
+    public int enemyC_Cost;
     public GameObject enemyD;
+    public int enemyD_Cost;
+
+    public GameObject Tester;
 
     public void SpawnEnemyA()
     {
-        Instantiate(enemyA, spawnPoint.position, Quaternion.identity);
+        if (MoneyManager.Instance.SpendMoney(enemyA_Cost))
+        {
+            Instantiate(enemyA, spawnPoint.position, Quaternion.identity);
+
+        }
+        else 
+        {
+
+            Debug.Log("not enoughMoeny");
+
+        }
     }
 
     public void SpawnEnemyB()
-    {
-        Instantiate(enemyB, spawnPoint.position, Quaternion.identity);
+    { 
+        if (MoneyManager.Instance.SpendMoney(enemyB_Cost))
+        {
+            Instantiate(enemyB, spawnPoint.position, Quaternion.identity);
+        }
+        else 
+        {
+
+            Debug.Log("not enoughMoeny");
+        
+        }    
     }
 
     public void SpawnEnemyC()
     {
-        Instantiate(enemyC, spawnPoint.position, Quaternion.identity);
+        if (MoneyManager.Instance.SpendMoney(enemyC_Cost))
+        {
+            Instantiate(enemyC, spawnPoint.position, Quaternion.identity);
+        }
+        else
+        {
+
+            Debug.Log("not enoughMoeny");
+
+        }
     }
 
     public void SpawnEnemyD()
     {
-        Instantiate(enemyD, spawnPoint.position, Quaternion.identity);
+        if (MoneyManager.Instance.SpendMoney(enemyD_Cost))
+        {
+            Instantiate(enemyD, spawnPoint.position, Quaternion.identity);
+        }
+        else
+        {
+
+            Debug.Log("not enoughMoeny");
+
+        }
+    }
+
+    public void SpawnTest()
+    {
+
+        Instantiate(Tester, spawnPointTest.position, Quaternion.identity);
+
     }
 }
